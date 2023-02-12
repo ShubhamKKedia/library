d6de6019103968ad8b09c3395a8a41f69e3ae4a1
shubham kedia <subham.kedia@oriserve.com>
2023-02-12 12:28:29 +0530

    hotfix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React(you can also try our beta website)
         </a>
         <br/>
         <button>Click Me!</button>
